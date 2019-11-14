a94f7050610b882daae54c9e6c4b83df58e589e9
pranbhat <pranav.bhat@microsoft.com>
2019-11-14 10:46:16 +0530

    Adding the code for findElementInArrayOccuringOddNumberOfTimes

---
+++ ideserve_online/findElementInArrayOccuringOddTimes.py
@@ -0,0 +1,29 @@
+import unittest
+import functools
+
+'''
+In an array having positive integers, except for one number which occurs odd number of times, all other numbers occur even number of times. Find the number.
+Example -  
+Input : 2 5 9 1 5 1 8 2 8
+Output: 9
+
+Input : 2 3 4 3 1 4 5 1 4 2 5
+Output: 4
+'''
+
+def findElementInArrayOccuringOddNumberOfTimes(arr):
+    return  functools.reduce(lambda x,y : x^y, arr, 0)
+
+
+class CodeTest(unittest.TestCase):
+    def testEmptyArray(self):
+        self.assertEqual(findElementInArrayOccuringOddNumberOfTimes([]), 0)
+
+    def testAllEvenCountArray(self):
+        self.assertEqual(findElementInArrayOccuringOddNumberOfTimes([1,1,3,3,2,2,6,6,4,4,3,3,5,5,7,7]), 0)
+
+    def testSingleElementOddCountArray(self):
+        self.assertEqual(findElementInArrayOccuringOddNumberOfTimes([1,1,3,3,2,2,6,6,4,4,3,3,5,5,5,7,7]), 5)
+
+
+unittest.main()
