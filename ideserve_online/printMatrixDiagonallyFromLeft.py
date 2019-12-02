927784055a91dce2d7315e1f0c886a79669f5218
Pranav Bhat <pranav.bhat@microsoft.com>
2019-12-02 12:08:41 +0530

    printMatrixDiagonallyFromLeft

---
+++ ideserve_online/printMatrixDiagonallyFromLeft.py
@@ -0,0 +1,71 @@
+from unittest import TestCase, main
+
+
+def printMatrixDiagonallyFromLeft(mat):
+    res = []
+    i = -1
+    rows = len(mat)
+    if rows:
+        cols = len(mat[0])
+    else:
+        cols = 0
+    for row in range(rows):
+        r = row
+        c = 0
+        i += 1
+        res.append([])
+        while r >= 0 and c < cols:
+            res[i].append(mat[r][c])
+            r -= 1
+            c += 1
+    for col in range(1, cols):
+        r = rows - 1
+        c = col
+        i += 1
+        res.append([])
+        while r >= 0 and c < cols:
+            res[i].append(mat[r][c])
+            r -= 1
+            c += 1
+
+    return res
+
+
+class CodeTest(TestCase):
+    def testEmpty(self):
+        self.assertEqual(printMatrixDiagonallyFromLeft([]), [])
+
+    def testSquareMatrix(self):
+        mat = [[1, 2, 3],
+               [4, 5, 6],
+               [7, 8, 9]]
+        expected = [[1],
+                    [4, 2],
+                    [7, 5, 3],
+                    [8, 6],
+                    [9]]
+        self.assertEqual(printMatrixDiagonallyFromLeft(mat), expected)
+
+    def testTowerMatrix(self):
+        mat = [[1, 2],
+               [4, 5],
+               [7, 8]]
+        expected = [[1],
+                    [4, 2],
+                    [7, 5],
+                    [8]]
+        self.assertEqual(printMatrixDiagonallyFromLeft(mat), expected)
+
+    def testSlabMatrix(self):
+        mat = [[1, 2, 3, 4, 5],
+               [6, 7, 8, 9, 10]]
+        expected = [[1],
+                    [6, 2],
+                    [7, 3],
+                    [8, 4],
+                    [9, 5],
+                    [10]]
+        self.assertEqual(printMatrixDiagonallyFromLeft(mat), expected)
+
+
+main()
